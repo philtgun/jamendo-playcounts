d99525d718e9d530eacd0248006da7385ba36fa7
Philip Tovstogan <phil.tgun@gmail.com>
2022-03-28 17:49:39 +0200

    Add tsv file sorted by playcounts

--- popular.py
+++ popular.py
@@ -3,14 +3,15 @@ from operator import index
 from pathlib import Path
 import pandas as pd
 
-def get_popular(tsv_file: Path, total: int = 20):
+def get_popular(tsv_file: Path, out_file: Path):
     df = pd.read_csv(tsv_file, sep='\t')
     df_sorted = df.sort_values(by='rate_listened_total', ascending=False)
-    print(df_sorted[:total])
+    df_sorted.to_csv(out_file, sep='\t')
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('tsv_file', type=Path) 
+    parser.add_argument('out_file', type=Path) 
     args = parser.parse_args()
 
-    get_popular(args.tsv_file)
+    get_popular(args.tsv_file, args.out_file)
